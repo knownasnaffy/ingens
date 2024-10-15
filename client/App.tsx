d877dcbeb84748658fba56cd3ba73a6b5af53384
Naffy Dharni <naffydharni006@gmail.com>
2024-10-15 20:52:29 +0530

    add bot avatar to web client

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center text-center"></div>
+      <div className="min-h-screen bg-zinc-800 flex flex-col gap-4 items-center justify-center text-center">
+        <img src='https://cdn.discordapp.com/avatars/1293594514912579635/9efc3ad7b9416a89fb586a1da382d383.webp?size=512' className="size-40 rounded-full border-8 border-zinc-700" />
+        <h1 className="text-3xl font-bold text-white inline-flex items-center">
+          <span className="bg-green-400 h-fit rounded-full p-1.5 mr-2 translate-y-0.5"></span>
+          Ingens#9193
+        </h1>
+      </div>
     </>
   );
 }
